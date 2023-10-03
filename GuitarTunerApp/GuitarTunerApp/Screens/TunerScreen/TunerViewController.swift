f85370db57927047713cbcd6b75506dee18860b6
artistic_mammoth <thrl0ad@ya.ru>
2023-10-03 16:09:21 +0300

    init project + basic setup

---
+++ GuitarTunerApp/GuitarTunerApp/Screens/TunerScreen/TunerViewController.swift
@@ -0,0 +1,19 @@
+//
+//  TunerViewController.swift
+//  GuitarTunerApp
+//
+//  Created by Михайлов Александр on 03.10.2023.
+//
+
+import UIKit
+
+class TunerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+
+
+}
+
